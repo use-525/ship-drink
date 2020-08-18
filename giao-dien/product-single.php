ff6344f636d729f549a317673d8656fce728252a
use-525 <65867519+use-525@users.noreply.github.com>
2020-08-18 12:45:05 +0700

    Update product-single.php

--- giao-dien/product-single.php
+++ giao-dien/product-single.php
@@ -251,4 +251,24 @@ $ccc = list_all_comment();
             <?php endforeach; ?>
         </div>
     </div>
+	<div class="modal fade" id="exampleModal" id="showTb" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-danger" id="exampleModalLabel">Thông báo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+     BẠN THÊM SẢN PHẨM THÀNH CÔNG
+      </div>
+      <!-- <div class="modal-footer"> -->
+        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      <!-- </div> -->
+    </div>
+  </div>
+</div>
+
 </section>
